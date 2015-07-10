03453f614e79e00c2871594a2f3672378a080a4c
Nicole Nigro <nicolenigro2000@gmail.com>
2015-07-10 16:27:17 +0000

    added erb

--- application_controller.rb
+++ application_controller.rb
@@ -1,13 +1,87 @@
-
 require 'bundler'
 Bundler.require
+
 require_relative './models/sf.rb'
 
 class ApplicationController < Sinatra::Base
-
   
   get '/' do
-    golden_gate_bridge = Attraction.new("Golden Gate Bridge", "24 hours", "7 days", 0)
+    erb :index
+  end
+  
+  get 'academy_of_sciences' do
+    erb :academy_of_sciences
+  end
+  
+  get '/alcatraz' do
+    erb :alcatraz
+  end
+  
+  get '/angel_island' do
+    erb :angel_island
+  end
+  
+  get '/att_park' do
+    erb :att_park
+  end
+  
+  get '/cable_car_museum' do
+    erb :cable_car_museum
+  end
+  
+  get '/chinatown' do
+    erb :chinatown
+  end
+  
+  get 'crissy_field' do
+    erb :crissy_field
+  end
+  
+  get '/de_young' do
+    erb :de_young
+  end
+  
+  get '/exploratorium' do
+    erb :exploratorium
+  end
+  
+  get 'fishermans_wharf' do
+    erb :fishermans_wharf
+  end
+  
+  get'/golden_gate_bridge' do
+    erb :golden_gate_bridge
+  end
+  
+  get 'golden_gate_park' do
+    erb :golden_gate_park
+  end
+  
+  get '/legion_of_honor' do
+    erb :legion_of_honor
+  end
+  
+  get '/pier_39' do
+    erb :pier_39
+  end
+  
+  get '/presidio' do
+    erb :presidio
+  end
+  
+  get '/sf_moma' do
+    erb :sf_moma
+  end
+  
+  get 'sf_zoo' do
+    erb :sf_zoo
+  end
+  
+  get 'yerba_buena' do
+    erb :yerba_buena
+  end
+   
+   golden_gate_bridge = Attraction.new("Golden Gate Bridge", "24 hours", "7 days", 0)
     pier_39 = Attraction.new("Pier 39", "10AM to 11PM", "7 days", 0)
     golden_gate_park = Attraction.new("Golden Gate Park", "24 hours", "7 days", 0)
     fishermans_wharf = Attraction.new("Fisherman's Wharf", "24 hours", "7 days", 0)
@@ -26,6 +100,5 @@ class ApplicationController < Sinatra::Base
     sf_zoo = Attraction.new("San Francisco Zoo", "10AM to 5PM", "7 days", 20)
     chinatown = Attraction.new("Chinatown", "24 hours", "7 days", 0)
     @attractions_array_alphabet = [academy_of_sciences, alcatraz, angel_island, att_park, cable_car_museum, chinatown, crissy_field, de_young, exploratorium, fishermans_wharf, golden_gate_bridge, golden_gate_park, legion_of_honor, pier_39, presidio, sf_moma, sf_zoo, yerba_buena]
-    erb :index
-  end
+  
 end
